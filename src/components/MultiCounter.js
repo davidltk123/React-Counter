c0d581b0c93731b8a91fb3bf974286a8176b8898
davidltk123 <davidltk123@gmail.com>
2020-12-08 17:32:54 +0800

    [React Counter][David] refactor: remove useless code for multicounter

--- src/components/MultiCounter.js
+++ src/components/MultiCounter.js
@@ -4,22 +4,8 @@ import CounterSizeGeneratorContainer from '../containers/CounterSizeGeneratorCon
 import CounterGroupSumContainier from '../containers/CounterGroupSumContainer';
 
 class MultiCounter extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { size: 0, sum: 0 };
-    }
-
-    updateSize = (size) => {
-        this.setState({ size: size, sum: 0 });
-    };
-
-    updateSum = (value) => {
-        this.setState({ sum: this.state.sum + value });
-    };
 
     render() {
-        const size = this.state.size;
-
         return (
             <div>
                 <CounterSizeGeneratorContainer />
